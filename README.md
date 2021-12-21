c37b110cda59b3a2b140f814d09e3cf03bd03809
Han Wang <han.wang@paladin.insure>
2021-12-20 17:11:43 -0800

    Initial commit with working build

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# log4jprotector
+# Log4J Immunizer
+
 Browser extension to prevent Log4J exploits against services running on private network and localhost.
+
+## How to install
+
+### Install from browser store
+
+We have submitted this extension to the Chrome, Edge and Mozilla stores and will update the sections below as the extension passes review and are made available to the public.
+
+#### Chrome
+
+Submitted 12/20/2021, will update link when available.
+
+#### Edge
+
+Submitted 12/20/2021, will update link when available.
+
+#### Mozilla
+
+Submitted 12/20/2021, will update link when available.
+
+#### Building from source
+
+1. Clone this repo via git
+2. Install dependencies with `npm ci`
+3. Build typescript to Javascript with `npm run build`
+4. Install this extension in developer mode by 
+
+## License
+
+See `/LICENSE`.
---
+++ src/background.ts
@@ -0,0 +1,44 @@
+'use strict';
+
+import { isPrivate } from 'ip';
+
+function requestProtection(
+    details : chrome.webRequest.WebRequestBodyDetails
+): { redirectUrl?: string, cancel?: boolean } {
+    // Remove query parameters so we don't trigger on search engines
+    const thisURL = new URL(details.url);
+    // We want to disable websocket connections to localhost and private IPs
+    // Kill all local and private IP traffic unless the initator is on the private network
+
+    if (['wss:', 'ws:'].includes(thisURL.protocol) 
+        && details.initiator 
+        && details.initiator !== 'null') {
+        const thisInitiator = new URL(details.initiator);
+
+        const hostnamePrivate = isPrivate(thisURL.hostname) || thisURL.hostname === 'localhost';
+        const initiatorPrivate = isPrivate(thisInitiator.hostname) || thisInitiator.hostname === 'localhost';
+        if ( hostnamePrivate && !initiatorPrivate) {
+            console.log('Blocked ', thisInitiator.origin, ' from accessing websockets on ', thisURL.origin);
+            return { cancel: true };
+        }
+  }
+
+  return {};
+}
+
+let hasListener = chrome.webRequest.onBeforeRequest.hasListener(requestProtection);
+
+function main() {
+    if (!hasListener) {
+            chrome.webRequest.onBeforeRequest.addListener(requestProtection, { urls: ['<all_urls>'] }, ['blocking']);
+    }    
+}
+
+// Ensure we register listener on install
+chrome.runtime.onInstalled.addListener(details => {
+   main();     
+});
+
+// Initialize main program logic 
+main();
+
